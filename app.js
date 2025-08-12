eb43261de236dd1d31a29619916e2e2268ed394e
jlgigio <joseluis.suarez73@gmail.com>
2025-08-11 20:58:26 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log(Ejecutando el sistema")
+let amigos = []
+let ingresarNombre = document.getElementById("amigo")
+let listaAmigos = document.getElementById("listaAmigos") 
+let resultado = document.getElementById("resultado")
+
+function agregarAmigo(){
+    if (ingresarNombre.value==="") {
+        alert("Porfavor, inserte un nombre")
+    } else {
+        amigos.push(ingresarNombre.value);
+        ingresarNombre.value="";
+        recorrerArray();
+        console.log(amigos)
+    }
+}
+function recorrerArray(){
+    listaAmigos.innerHTML="";
+    for(let i=0; i < amigos.length; i++){
+        let nombresLista = document.createElement("li");
+        nombresLista.textContent=amigos[i];
+        listaAmigos.appendChild(nombresLista);
+
+    }
+    
+}
+
+function sortearAmigo(){
+    if(amigos===""){
+        alert("Ingresar nombres");
+    }else{
+        let posicionAleatoria = Math.floor(Math.random()*amigos.length)
+        let ganador = amigos[posicionAleatoria]
+        console.log(ganador)
+        resultado.innerHTML= `El amigo secreto es ${ganador}`;
+    }
+}
